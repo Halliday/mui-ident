4e56c13903e38839b44a3a5590f037b25ea602b8
Johann Forster <44815456+Halliday@users.noreply.github.com>
2022-11-11 13:00:39 +0100

    Update deps

--- src/components/Language.tsx
+++ src/components/Language.tsx
@@ -1,15 +1,18 @@
 import { ArrowDropDown } from "@mui/icons-material";
 import { Button, ButtonProps, ListItemIcon, ListItemText, Menu } from "@mui/material";
 import MenuItem from "@mui/material/MenuItem";
-import React, { useState } from "react";
-import { setLanguage, useLanguage } from "@halliday/react-i18n";
+import React, { useEffect, useState } from "react";
+import { Language, loadLanguages, setLanguage, useLanguage } from "@halliday/react-i18n";
 
 interface SelectLanguageProps extends ButtonProps {
 
 }
 
 export function SelectLanguage(props: SelectLanguageProps) {
-    const lang = useLanguage();
+    const code = useLanguage();
+
+    const [languages, setLanguages] = useState<Language[]>([]);
+    useEffect(() => { loadLanguages().then(setLanguages); }, []);
 
     const [anchorEl, setAnchorEl] = useState<Element | null>(null);
     function handleButtonClick(ev: React.MouseEvent<Element>) {
@@ -18,18 +21,26 @@ export function SelectLanguage(props: SelectLanguageProps) {
     function handleClose() {
         setAnchorEl(null);
     }
+
+    if (languages.length === 0)
+        return null;
+
     const open = anchorEl !== null;
-    const Flag = languages.find(l => l.id === lang)!.Flag;
+
+    const currentLanguage = languages.find(l => l.code === code)!;
+
     return <>
         <Button onClick={handleButtonClick} color="inherit" style={{backgroundColor: open ? "grey.100" : undefined}} {...props}>
-            <Flag />
+            <img src={currentLanguage.flag} alt={currentLanguage.name} style={{height: "18px", width: "30px"}} />
             <ArrowDropDown />
         </Button>
         <Menu open={open} anchorEl={anchorEl} onClose={handleClose}>
             {
                 languages.map(l => (
-                    <MenuItem key={l.id} value={l.id} dense onClick={() => setLanguage(l.id)} selected={l.id === lang}>
-                        <ListItemIcon sx={{ mr: 2 }}><l.Flag /></ListItemIcon>
+                    <MenuItem key={l.code} value={l.code} dense onClick={() => setLanguage(l.code)} selected={l.code === code}>
+                        <ListItemIcon sx={{ mr: 2 }}>
+                            <img src={l.flag} alt={l.name} style={{height: "18px", width: "30px"}} /> 
+                        </ListItemIcon>
                         <ListItemText primary={l.name} secondary={l.country} />
                     </MenuItem>
                 ))
@@ -46,39 +57,4 @@ export function LanguageContext(props: LanguageContextProps) {
     const children = props.children;
     const lang = useLanguage();
     return <React.Fragment key={lang}>{children}</React.Fragment>;
-}
-
-type Language = {
-    id: string,
-    name: string,
-    country: string,
-    Flag:React.ComponentType
-}
-
-const languages: Language[] = [{
-    id: "en",
-    name: "English",
-    country: "United States",
-    Flag: FlagUS,
-}, {
-    id: "de",
-    name: "German",
-    country: "Deutschland",
-    Flag: FlagDE,
-}];
-
-function FlagDE() {
-    return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 5 3" width={30} height={18}>
-        <path d="M0 0h5v3H0z" />
-        <path fill="#D00" d="M0 1h5v2H0z" />
-        <path fill="#FFCE00" d="M0 2h5v1H0z" />
-    </svg>;
-}
-
-function FlagUS() {
-    return <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 7410 3900" width={30} height={18}>
-        <path fill="#b22234" d="M0 0h7410v3900H0z" />
-        <path d="M0 450h7410m0 600H0m0 600h7410m0 600H0m0 600h7410m0 600H0" stroke="#fff" strokeWidth="300" />
-        <path fill="#3c3b6e" d="M0 0h2964v2100H0z" />
-    </svg>;
 }
